10ad385da7dea1c73fedcb6030b1af982af4ed80
Tobias Odermatt <odermatt.tobias.w@gmail.com>
2024-07-07 13:02:13 +0200

    singup done

--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.core import serializers
 
@@ -14,8 +15,8 @@ def index(request):
         print("Received data" + request.POST['textmessage'])
         myChat = Chat.objects.get(id=1)
         new_Message = Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
-        serialized_obj = serializers.serialize('json', [new_Message]);
-        return JsonResponse(serialized_obj[1:-1], safe=False);
+        serialized_obj = serializers.serialize('json', [new_Message])
+        return JsonResponse(serialized_obj[1:-1], safe=False)
     chatMessages = Message.objects.filter(chat__id=1) # chat__id=1 > Man schaut von der Message auf das Objekt Chat mit der id 1
     return render(request, 'chat/index.html', {'messages': chatMessages})
 
@@ -24,24 +25,37 @@ def loginView(request):
     #redirect = request.GET.get('next')
     if request.method == 'POST':
         user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
-        print(user)
         if user:
             login(request, user)
-            return HttpResponseRedirect('/chat/')
-        else:
-            wrongPassword = True
-            return render(request, 'chat/login.html', {'wrongpassword': wrongPassword})
-    return render(request, 'chat/login.html')
+            serialized_obj = serializers.serialize('json', [user])
+            return JsonResponse(serialized_obj[1:-1], safe=False)
+            #return HttpResponseRedirect('/chat/')
+        #else:
+        #    wrongPassword = True
+            #serialized_obj = serializers.serialize('json', [user])
+            #return JsonResponse(serialized_obj[1:-1], safe=False)
+
+        #    return render(request, 'chat/login.html', {'wrongpassword': wrongPassword})  
+    return render(request, 'chat/login.html')    
 
 def signUpView(request):
-    redirect = request.GET.get('next')
-    form = UserCreationForm(request.POST)
-
+    #redirect = request.GET.get('next')
     if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/chat/')
-            #return redirect('login')
-        else:
-            form = UserCreationForm()
-    return render(request, 'chat/signUp.html', {'form': form, 'redirect': redirect})
+        username = request.POST['username']
+        user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+        print(user)
+        print(username)
+        print(User.objects.get(username=username))
+
+        try:
+            User.objects.get(username=username)
+            serialized_obj = serializers.serialize('json', [user])
+            return JsonResponse(serialized_obj[1:-1], safe=False)
+        except:
+            user.save()
+            login(request, user)
+            serialized_obj = serializers.serialize('json', [user])
+            print(serialized_obj)
+            return JsonResponse(serialized_obj[1:-1], safe=False)
+        
+    return render(request, 'chat/signUp.html')
